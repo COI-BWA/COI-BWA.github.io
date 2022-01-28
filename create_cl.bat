dff32e26b9adc286f7526541ac02e21250c3aa7b
Friso Kluit <friso_kluit@rp.edu.sg>
2022-01-28 15:37:03 +0800

    Reset

--- create_cl.bat
+++ create_cl.bat
@@ -5,6 +5,8 @@ claat export 19-Tp3FRROgGvmDqXA51rVpsvxOdtu8ppKVnWyKPs4Gc
 claat export 1cnVE64OoAxwsuDJHRW83LqKiBrTfpE6nyReBBl3hHzw
 claat export 1TwtpBJStqkXhkvECoVjHE0xfbFNNCMk04pbbKm4_c9Q
 
+gulp dist
+
 git add .
 
 git commit -m auto-upload
