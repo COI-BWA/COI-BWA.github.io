cdb3a0a278e513d76e84a8444787a8bdd5d2570b
Friso Kluit <friso_kluit@rp.edu.sg>
2022-01-28 14:57:17 +0800

    20220128

---
+++ create_cl.bat
@@ -0,0 +1,6 @@
+@echo off
+claat export 1lYw56kQSx1Nq9WLipQX3fbn2YnHb3q9Y0-epk4n_X6o
+claat export 1fRpnPDoqLPqeoj5EmMGMMrw5Vx5aHRQQn6mRbZgxh9o
+claat export 19-Tp3FRROgGvmDqXA51rVpsvxOdtu8ppKVnWyKPs4Gc
+claat export 1cnVE64OoAxwsuDJHRW83LqKiBrTfpE6nyReBBl3hHzw
+claat export 1TwtpBJStqkXhkvECoVjHE0xfbFNNCMk04pbbKm4_c9Q
